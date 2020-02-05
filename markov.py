7320c33814d31db33294e27dfb3cc3532c09e86c
Your Name <you@example.com>
2020-02-05 12:18:52 -0800

    took out # comments of code that didn't work

--- markov.py
+++ markov.py
@@ -10,45 +10,18 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
     contents = open(file_path).read()
-    # read_file = open_file.read()
-    # print(read_file)
-    # read_file = str(read_file)
-    # print(read_file)
+   
     words = contents.split()
     first_dict = {}
-    # appended_dict = {}
-    
-    # key_value_paired = []
-
-    # key_variables = [(words[word], words[word + 1], )]
+   
     for idx in range(len(words) - 2):
-        # if word in words:
-        # value_list = [words[idx]]
+       
         key_variables = (words[idx], words[idx + 1], )
-        # first_dict[key_variables] = value_list
-    # print(first_dict)
-        # if key_variables in words:
         first_dict[key_variables] = first_dict.get(key_variables, [])
         first_dict[key_variables].append(words[idx + 2])
-        # print(value_list)
-            # appended_dict[key_variables] = value_list.append(word) 
-            # print(appended_dict)
-        # else:
-        #  first_dict[key_variables] = value_list.append(words[word]) # dict obj has no attr 'append'
-        # for key_variables in words:
-        #     appended_dict[words[word], words[word + 1]] = value_list.append([words[word] + (words[word])])            
-        #new_tuples = (words[word], words[word + 1])
-        # # print(new_tuples)
-        # for new_tuples in first_dict:
-        # import pdb; pdb.set_trace()
-        # first_dict = {(words[word], words[word + 1]) : words[word+2]}
-    # print(value_list)
-    print(first_dict)
-    # print(value_list)
-    # for new_tuples in words:
-    #     first_dict = 
-    # loop through our list of words, append any value that corresponds to each 
-    # key value as a list for each key
+        
+    # print(first_dict)
+    
     return str(first_dict)
 
 
